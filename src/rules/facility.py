a7917ab74a59d9c4d6aecd6d3b7efebe6d65c388
steveoh <sgourley@utah.gov>
2019-03-27 17:10:13 -0600

    chore: remove unused classes

--- src/rules/facility.py
+++ src/rules/facility.py
@@ -5,42 +5,8 @@ facility.py
 A module that creates attribute rules for the UICFacility table
 '''
 
-import os
-
-from models.rule import Rule
+from config import config
 from models.ruletypes import Calculation, Constant, Constraint
-
-
-class CalculateGuidRule(Rule):
-
-    def __init__(self, sde, table, metas):
-        super(CalculateGuidRule, self).__init__()
-        self.name = table
-        self.table_path = os.path.join(sde, table)
-        self.meta_rules = metas
-
-        self.tag = 'GUID'
-
-        self.error_number = 7500
-        self.error_message = 'This GUID is auto generated and cannot be modified.'
-
-        self.arcade = 'return Guid()'
-
-
-class CalculateConstantRule(Rule):
-
-    def __init__(self, sde, table, metas):
-        super(CalculateConstantRule, self).__init__()
-
-        self.name = table
-        self.table_path = os.path.join(sde, table)
-        self.meta_rules = metas
-
-        self.tag = 'Constant'
-
-        self.error_number = 7501
-        self.error_message = 'This value is auto generated and cannot be modified.'
-
 
 extract_fips = '''var set = FeatureSetByName($datastore, 'Counties')
 function getAttributeFromLargestArea(feat, set, field) {
