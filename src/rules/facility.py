545c6fa05949b0c73ed796fe87f447341d01f411
steveoh <sgourley@utah.gov>
2019-04-01 13:21:27 -0600

    fix: error message must be only key

--- src/rules/facility.py
+++ src/rules/facility.py
@@ -118,15 +118,13 @@ return getAttributeFromLargestArea($feature, set, field);
 constrain_domain = '''var code = number($feature.countyfips)
 if (isnan(code)) {
     return {
-        'errorMessage': 'The fips code is empty',
-        'result': false
+        'errorMessage': 'The fips code is empty'
     };
 }
 
 if (code % 2 == 0) {
     return {
-        'errorMessage': 'The fips code is should be odd: ' + code,
-        'result': false
+        'errorMessage': 'The fips code is should be odd: ' + code
     };
 }
 
@@ -135,8 +133,7 @@ if (code >= 49001 && code <= 49057) {
 }
 
 return {
-    'errorMessage': 'The code does not fall within the valid ranges: ' + code,
-    'result': false
+    'errorMessage': 'The code does not fall within the valid ranges: ' + code
 };
 '''
 
