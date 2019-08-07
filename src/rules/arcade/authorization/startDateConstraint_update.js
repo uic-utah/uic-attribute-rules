8751ce9c53ac9113737bd0e90b66a5031aedf46b
steveoh <sgourley@utah.gov>
2019-08-07 16:38:29 -0600

    fix(authorization): invert condition

--- src/rules/arcade/authorization/startDateConstraint_update.js
+++ src/rules/arcade/authorization/startDateConstraint_update.js
@@ -2,6 +2,6 @@ if (!haskey($feature, 'startdate')) {
     return true;
 }
 
-return iif(!isempty($feature.startdate), {
+return iif(isempty($feature.startdate), {
     'errorMessage': 'Authorization start date must not be empty. Input: ' + $feature.startdate
 }, true);
