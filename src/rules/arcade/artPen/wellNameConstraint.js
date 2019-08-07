c56fba96f2e80c7985b49c6194dc6b2f25827ffc
steveoh <sgourley@utah.gov>
2019-08-07 16:33:52 -0600

    fix(art_pen): fix backwards boolean

--- src/rules/arcade/artPen/wellNameConstraint.js
+++ src/rules/arcade/artPen/wellNameConstraint.js
@@ -2,6 +2,6 @@ if (!haskey($feature, 'ArtPen_WellName')) {
     return true;
 }
 
-return iif(!isempty($feature.artpen_wellname), {
+return iif(isempty($feature.artpen_wellname), {
     'errorMessage': 'ArtPen well name must not be empty. Input: ' + $feature.artpen_wellname
 }, true);
