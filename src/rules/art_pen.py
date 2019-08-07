34e964ef1c3bd4f6077afee51bffc9d9872592f6
steveoh <sgourley@utah.gov>
2019-08-06 20:35:09 -0600

    fix(art_pen): use folder name not table

--- src/rules/art_pen.py
+++ src/rules/art_pen.py
@@ -12,22 +12,23 @@ from services.loader import load_rule_for
 from . import common
 
 TABLE = 'UICArtPen'
+FOLDER = 'artPen'
 
 guid_constraint = Constant('Art Pen Guid', 'GUID', 'ArtPen.Guid', 'GUID()')
 
-name_constraint = Constraint('Art Pen Well Name', 'ArtPen.ArtPen_WellName', load_rule_for(TABLE, 'wellName'))
+name_constraint = Constraint('Art Pen Well Name', 'ArtPen.ArtPen_WellName', load_rule_for(FOLDER, 'wellName'))
 name_constraint.triggers = [config.triggers.insert, config.triggers.update]
 
 well_type_constraint = Constraint('Art Pen Well Type', 'ArtPen.WellType', common.constrain_to_domain('welltype', allow_null=False, domain='UICArtPenWellType'))
 well_type_constraint.triggers = [config.triggers.insert, config.triggers.update]
 
-review_date_constraint = Constraint('Art Pen Review Date', 'ArtPen.Artpen_ReviewDate', load_rule_for(TABLE, 'reviewDateConstraint'))
+review_date_constraint = Constraint('Art Pen Review Date', 'ArtPen.Artpen_ReviewDate', load_rule_for(FOLDER, 'reviewDateConstraint'))
 review_date_constraint.triggers = [config.triggers.insert, config.triggers.update]
 
-catype_constraint = Constraint('Art Pen Review Date', 'ArtPen.ArtPen_CAType', load_rule_for(TABLE, 'caTypeConstraint'))
+catype_constraint = Constraint('Art Pen Review Date', 'ArtPen.ArtPen_CAType', load_rule_for(FOLDER, 'caTypeConstraint'))
 catype_constraint.triggers = [config.triggers.insert, config.triggers.update]
 
-cadate_constraint = Constraint('CA Date', 'ArtPen.ArtPen_CADate', load_rule_for(TABLE, 'caDateConstraint'))
+cadate_constraint = Constraint('CA Date', 'ArtPen.ArtPen_CADate', load_rule_for(FOLDER, 'caDateConstraint'))
 cadate_constraint.triggers = [config.triggers.insert, config.triggers.update]
 
 RULES = [
