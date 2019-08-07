87822aa4600153c1375bdc6eac9592297d4c227b
steveoh <sgourley@utah.gov>
2019-08-07 15:07:58 -0600

    fix(art_pen): ident4ca and review date are linked

--- src/rules/arcade/artPen/reviewDateConstraint.js
+++ src/rules/arcade/artPen/reviewDateConstraint.js
@@ -3,10 +3,16 @@ if (!haskey($feature, 'artpen_reviewdate') || !haskey($feature, 'ident4ca')) {
     return true;
 }
 
-if (isempty($feature.ident4ca)) {
+if (isempty($feature.ident4ca) && isempty($feature.artpen_reviewdate)) {
     return true;
 }
 
+if (isempty($feature.ident4ca)) {
+    return {
+        'errorMessage': 'Ident4CA may not be empty if ArtPen_ReviewDate has a value.'
+    }
+}
+
 return iif(isempty($feature.artpen_reviewdate), {
-    'errorMessage': 'Review date is required when Ident4CA has a value.'
+    'errorMessage': 'ArtPen_ReviewDate may not be empty if Ident4CA has a value.'
 }, true);
