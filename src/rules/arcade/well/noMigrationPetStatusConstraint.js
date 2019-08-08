fd2486e7641474cd2a5b6a4434b1d788fa3d03e2
steveoh <sgourley@utah.gov>
2019-08-08 10:26:42 -0600

    fix(well): domain code syntax error

--- src/rules/arcade/well/noMigrationPetStatusConstraint.js
+++ src/rules/arcade/well/noMigrationPetStatusConstraint.js
@@ -8,6 +8,6 @@ if ($feature.wellsubclass != 1001) {
     return true;
 }
 
-return iif(isempty($feature.nomigrationpetstatus) || domaincode($feature, 'nomigrationpetstatus') == 'NA', {
+return iif(isempty($feature.nomigrationpetstatus) || domaincode($feature, 'nomigrationpetstatus', $feature.nomigrationpetstatus) == 'NA', {
     'errorMessage': 'Class I Hazardous wells require a NoMigrationPetStatus.'
 }, true);
