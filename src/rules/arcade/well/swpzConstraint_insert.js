403846d1f50017f0ee7483a835afa68d9be3df34
steveoh <sgourley@utah.gov>
2019-08-27 12:45:58 -0600

    fix(well): update error messages
    closes #64
    closes #65

--- src/rules/arcade/well/swpzConstraint_insert.js
+++ src/rules/arcade/well/swpzConstraint_insert.js
@@ -3,5 +3,5 @@ if (!haskey($feature, 'wellswpz') || isempty($feature.wellswpz)) {
 }
 
 return iif(isempty(domainname($feature, 'wellswpz', $feature.wellswpz)), {
-    'errorMessage': 'WellSWPZ may not be <null>; select the appropriate value from the UICGWProtectionDomain (dropdown menu). Input: ' + $feature.wellswpz
+    'errorMessage': 'WellSWPZ may not be empty; select the appropriate value from the UICGWProtectionDomain (dropdown menu). Input: ' + $feature.wellswpz
 }, true);
