71724ea88dae73bd7f8c88a932afba47ef38d825
steveoh <sgourley@utah.gov>
2019-08-27 13:23:03 -0600

    fix(auth-action): error messages
    closes #76

--- src/rules/arcade/authorizationAction/typeConstraint.js
+++ src/rules/arcade/authorizationAction/typeConstraint.js
@@ -7,5 +7,5 @@ if (isempty($feature.authorizationactiontype)) {
 }
 
 return iif(isempty($feature.authorizationactiondate), {
-    'errorMessage': 'AuthorizationActionDate may not be null when AuthorizationActionType has a value'
+    'errorMessage': 'AuthorizationActionDate may not be empty when AuthorizationActionType has a value.'
 }, true);
