75dbf50aa5474b34f4d7295c0a2599fdaa908318
Ryan Parker <49735267+rsparker-utah@users.noreply.github.com>
2023-09-08 12:08:13 -0600

    Update idCalculation.js
    Code no longer in use. Generation of FacilityID field will be handled by the UIC web application (i.e., https://github.com/uic-utah/uic-inventory).

--- src/rules/arcade/facility/idCalculation.js
+++ src/rules/arcade/facility/idCalculation.js
@@ -1,3 +1,4 @@
+//Code no longer in use. Generation of FacilityID field will be handled by the UIC web application (i.e., https://github.com/uic-utah/uic-inventory)
 var keys = ['countyfips', 'guid'];
 
 for (var key in keys) {
