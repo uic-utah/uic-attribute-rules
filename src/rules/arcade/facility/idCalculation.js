d9ef17ed269339c82446445ea3a5d7597ae63fda
Ryan Parker <49735267+rsparker-utah@users.noreply.github.com>
2024-09-04 16:40:19 -0600

    Update idCalculation.js
    Removed note about the Facility ID being calculated by the UIC web application (https://github.com/uic-utah/uic-inventory). The Facility ID was planned to be calculated by the UIC web application, but requirements changed and so it was no longer required for the UIC web application to calculated the Facility ID.

--- src/rules/arcade/facility/idCalculation.js
+++ src/rules/arcade/facility/idCalculation.js
@@ -1,4 +1,3 @@
-//Code no longer in use. Generation of FacilityID field will be handled by the UIC web application (i.e., https://github.com/uic-utah/uic-inventory)
 var keys = ['countyfips', 'guid'];
 
 for (var key in keys) {
